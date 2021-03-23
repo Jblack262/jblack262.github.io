444150163185a73824929747a0111d767f75a47b
Nlvya <minecraftengine10@gmail.com>
2021-03-22 22:15:54 -0700

    deploy

---
+++ precache-manifest.9ca50ca7fbd1c411cbec8ab03bdbace1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4027567e17aeb94d677",
+    "url": "/Vue-portfolio/css/app.e90cc075.css"
+  },
+  {
+    "revision": "0d8b2420cb5efb043b8e18aac4768ac0",
+    "url": "/Vue-portfolio/index.html"
+  },
+  {
+    "revision": "c4027567e17aeb94d677",
+    "url": "/Vue-portfolio/js/app.11323e17.js"
+  },
+  {
+    "revision": "f9b4d57ea9851ad0c9bd",
+    "url": "/Vue-portfolio/js/chunk-vendors.f01202b3.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/Vue-portfolio/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Vue-portfolio/robots.txt"
+  }
+]);
